9482e61a251609c7fd19ea365a858b95576e5823
BrayanRojasLemus <brayanrojas@wposs.com>
2023-05-29 17:26:01 -0500

    Permisos a modulos con Cookies

---
+++ ROLES/Controllers/AccesoController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using ROLES.Models;
+using ROLES.Data;
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Claims;
+
+namespace ROLES.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Usuario _usuario)
+        {
+            DA_Logica _da_usuario = new DA_Logica();
+
+            var usuario = _da_usuario.ValidarUsuario(_usuario.Correo, _usuario.Clave);
+
+            if (usuario != null)
+            {
+                var claims = new List<Claim> {
+
+                    new Claim(ClaimTypes.Name, usuario.Nombre),
+                    new Claim("Correo", usuario.Correo)
+                };
+
+                foreach (string rol in usuario.Roles) {
+                    claims.Add(new Claim(ClaimTypes.Role, rol));
+                }
+
+                var claimsIndentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIndentity));
+
+                return RedirectToAction("Index", "Home");
+            }
+            else { 
+            
+                return View();
+            }
+
+        }
+
+
+        public async Task<IActionResult> Salir()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            return RedirectToAction("Index", "Acceso");
+        }
+    }
+}
---
+++ ROLES/Data/DA_Logica.cs
@@ -0,0 +1,33 @@
+﻿
+using ROLES.Models;
+
+namespace ROLES.Data
+{
+    public class DA_Logica
+    {
+        public List<Usuario> ListaUsuarios() 
+        {
+            return new List<Usuario> {
+
+                new Usuario{ Nombre = "Bryan", Correo= "BryanAdministrador@gmail.com", Clave = "123", Roles= new string[]{ "Administrador"} },
+                new Usuario{ Nombre = "Manuel", Correo= "ManuelSupervisor@gmail.com", Clave = "123", Roles= new string[]{ "Supervisor"} },
+                new Usuario{ Nombre = "Alejo", Correo= "AlejoEmpleado@gmail.com", Clave = "123", Roles= new string[]{ "Empleado"} },
+                new Usuario{ Nombre = "Estefano", Correo= "superEmpleado@gmail.com", Clave = "123", Roles= new string[]{ "Supervisor","Empleado"} },
+
+
+            };
+        
+        }
+
+
+        public Usuario ValidarUsuario(string _correo,string _clave) {
+
+            return ListaUsuarios().Where(item => item.Correo == _correo && item.Clave == _clave).FirstOrDefault();        
+        
+        }
+
+
+
+
+    }
+}
